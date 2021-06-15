37d7218e7ba538c194fb5d4b4392a9a0ad012eee
FaizalAzmanOUM <73531671+FaizalAzmanOUM@users.noreply.github.com>
2021-06-15 19:22:16 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(elt2m=pzt!xiazs72@bb7$s4yp=)!z#xno7eds+q_)8ywxgql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['demo-makmal-system.herokuapp.com']
 
 
 # Application definition
